d28f9d27aa21329cf919ce05dc9c1020a768f6b9
sully <csullivan@truelearn.com>
2023-06-08 21:21:40 -0400

    changed dependency to my code

--- TamperMonkey.js
+++ TamperMonkey.js
@@ -20,8 +20,8 @@
     script.type = 'text/javascript';
     link.type = 'text/css';
     link.rel = 'stylesheet';
-    script.src = 'https://cdn.jsdelivr.net/gh/xadam1/GrepoBot@v5.1/GrepobotMinified.js';
-    link.href = 'https://cdn.jsdelivr.net/gh/xadam1/GrepoBot@v5.1/Autobot.css';
+    script.src = 'https://cdn.jsdelivr.net/gh/sullyTheDev/autobot/GrepobotMinified.js';
+    link.href = 'https://cdn.jsdelivr.net/gh/xadam1/autobot/Autobot.css';
     head.appendChild(script);
     head.appendChild(link);
     head.setAttribute('xhttps', 1);
