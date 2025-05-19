f99c2c6ce3bfc4e056a05555438429dd63f788c7
Christopher Sullivan <31122729+sullyTheDev@users.noreply.github.com>
2025-05-19 11:40:01 -0400

    more stuff

--- Autobot-Cracked.js
+++ Autobot-Cracked.js
@@ -2,7 +2,7 @@ var Autobot = {
   title: 'GrepoBot',
   version: '5.1',
   domain: window['location']['protocol'] + '//bot.grepobot.com/',
-  scriptDomain: window['location']['protocol'] + '//cdn.jsdelivr.net/gh/sullyTheDev/autobot@1.2/',
+  scriptDomain: window['location']['protocol'] + '//cdn.jsdelivr.net/gh/sullyTheDev/autobot@1.3/',
   botWnd: '',
   botPremWnd: '',
   botEmailWnd: '',
--- install.user.js
+++ install.user.js
@@ -20,8 +20,8 @@
     script.type = 'text/javascript';
     link.type = 'text/css';
     link.rel = 'stylesheet';
-    script.src = 'https://cdn.jsdelivr.net/gh/sullyTheDev/autobot@1.2/GrepoBotMinified.js';
-    link.href = 'https://cdn.jsdelivr.net/gh/sullyTheDev/autobot@1.2/Autobot.css';
+    script.src = 'https://cdn.jsdelivr.net/gh/sullyTheDev/autobot@1.3/GrepoBotMinified.js';
+    link.href = 'https://cdn.jsdelivr.net/gh/sullyTheDev/autobot@1.3/Autobot.css';
     head.appendChild(script);
     head.appendChild(link);
     head.setAttribute('xhttps', 1);
